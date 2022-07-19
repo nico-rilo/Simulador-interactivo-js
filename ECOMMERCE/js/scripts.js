97d5d0f7391156b257c5451424cbee03b42f88a4
nico-rilo <nicolasrilo@live.com.ar>
2022-07-19 01:01:38 -0300

    modificacion en carpeta ECOMMERCE sub carpeta js archivo scripts.js

---
+++ ECOMMERCE/js/scripts.js
@@ -0,0 +1,9 @@
+const producto = "Remeras Rock";
+
+function agregarAlCarrito(){
+    console.log("Agregaste el producto: " + producto);
+}
+
+agregarAlCarrito()
+agregarAlCarrito()
+agregarAlCarrito()
