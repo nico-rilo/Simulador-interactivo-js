4e26f23578aee04e14fc4ffc6e3da0ddedebfbf1
nico-rilo <nicolasrilo@live.com.ar>
2022-08-08 22:12:26 -0300

    modificaciones en js y en index.html

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
-let numeroUno;
-let numeroDos;
-numeroUno = prompt("Indica tu nombre: ");
-numeroDos = prompt("Indica tu apellido: ");
 
-let resultado;
-resultado = numeroUno + numeroDos;
-document.write("Tu nombre y apellido en consecuencia conforman tu nombre usuario, bienvenido al mejor sitio de conversion: " + resultado);
-
-console.log(resultado);
-
-
+let nombre = prompt("Pone tu nombre");
+let numeroUser = alert("Pensá un número, pero no me lo digas");
+let numero1User = alert("Sumale 5");
+let numero2User = alert("Multiplicalo por 2");
+let numero3User = alert("Resta 4");
+let numero4User = alert("Dividilo por 2");
+let numero5User = alert("Restale el número que pensaste al principio");
+let numero1 = 5;
+let numero2 = 2;
+let numero3 = 4;
+let numero4 = 2;
+let resultado = (numero1 * numero2 - numero3) / numero4;
+let resultadoUser = alert("Te quedó " + resultado);
+let nombreUser = alert("Gracias por jugar " + nombre);
